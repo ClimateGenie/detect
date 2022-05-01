aa51c0ed4aadee698cd8eca3acae5eb4d5ff830a
Fonzzy1 <alfiechadwick@hotmail.com>
2022-05-01 22:06:40 +1000

    finished embedding scheme

--- embedding.py
+++ embedding.py
@@ -1,45 +1,51 @@
-from dataset import Dataset, istarmap
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import silhouette_score
+from dataset import Dataset
+from utils import *
+from random import choice, random
 import gensim
-import nltk
 import os
 import logging
-import random
-from multiprocessing import Manager, Pool
-import tqdm
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
 logging.basicConfig(format='%(message)s', level=logging.INFO)
+import collections
 import pickle
 
 
 class Embedding():
 
-    def __init__(self,training_data,tokenisation_function,dm=0,vect_size=200,window = 5, hs = 1, epochs = 3):
+    def __init__(self,training_data,dm=0,vect_size=200,window = 5, hs = 1, epochs = 20):
 
         self.model_string = f'embedding-{dm}-{vect_size}-{window}-{hs}-{epochs}.pickle'
         self.pickle_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'picklejar',self.model_string)
-        self.training_data = training_data
-        self.tokenisation_function = tokenisation_function
+        self.training_data = [gensim.models.doc2vec.TaggedDocument(word_token(x),[i]) for i,x in enumerate(training_data)]
 
-        if os.path.exists(self.pickle_path):
-            with open(self.pickle_path,'rb') as f:
-                self.model = pickle.load(f)
-        else:
-            self.model = gensim.models.doc2vec.Doc2Vec(vector_size=vect_size,dm=dm,window=window,hs=1,epochs = 3)
-            self.model.build_vocab(corpus_iterable = self.training_data)
-            self.model.train(self.training_data, total_examples=self.model.corpus_count, epochs=3)
-            with open(self.pickle_path,'wb') as f:
-                pickle.dump(self.model,f)
+        self.model = gensim.models.doc2vec.Doc2Vec(vector_size=vect_size,dm=dm,window=window,hs=1,epochs = epochs)
+        self.model.build_vocab(corpus_iterable = self.training_data)
+        self.model.train(self.training_data, total_examples=self.model.corpus_count, epochs = self.model.epochs)
 
 
-    def find_closest(self,sentence):
-        inferred_vector = self.model.infer_vector(self.tokenisation_function(sentence))
-        sims = self.model.dv.most_similar([inferred_vector], topn=1)
-        print(' '.join(self.training_data[sims[0][0]].words))
+    def find_closest(self,sentence, num = 1):
+        inferred_vector = self.model.infer_vector(word_token(sentence))
+        sims = self.model.dv.most_similar([inferred_vector], topn=num)
+        for i in range(len(sims)):
+            print(' '.join(self.training_data[sims[i][0]].words))
+
+    def validate(self):
+        ranks = []
+        for doc_id in range(len(self.training_data)):
+            inferred_vector = self.model.infer_vector(self.training_data[doc_id].words)
+            sims = self.model.dv.most_similar([inferred_vector], topn=len(self.model.dv))
+            rank = [docid for docid, sim in sims].index(doc_id)
+            ranks.append(rank)
+        count = collections.Counter(ranks)
+        print(count[0]/len(self.training_data))
+
+    def eye_test(self):
+        test = choice([x.words for x in self. training_data])
+        print(' '.join(test))
+        self.find_closest(' '.join(test), num = 5)
+
 
 if __name__ == "__main__":
     d = Dataset()
-    d.gather_embedding_training(sample = 0.3)
-    e = Embedding(d.embedding_train,d.word_token)
+    training_data = flatten(flatten([simple_map(sent_token,d.df_climate['article']), simple_map(sent_token, d.df_skeptics['article'])]))
+    e = Embedding(training_data)
+    e.eye_test()
