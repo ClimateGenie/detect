40fe8b01cefde79f1665472f471e0d9756951f0a
Fonzzy1 <alfiechadwick@hotmail.com>
2022-05-04 11:22:49 +1000

    I now track comments

--- dataset.py
+++ dataset.py
@@ -22,9 +22,10 @@ class Dataset():
             self.load()
         except FileNotFoundError:
             self.timestamps=[]
-            self.df_climate = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score'])
-            self.df_news = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score'])
-            self.df_skeptics = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score'])
+            self.df_climate = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score','comments'])
+            self.df_news = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score','comments'])
+            self.df_skeptics = pd.DataFrame(columns= ['author','timestamp','post_url','media_url','score','comments'])
+            self.df_sentence = pd.DataFrame(columns=['parent', 'sentence'])
 
         utc = pytz.UTC
         day = utc.localize(from_date)
@@ -45,39 +46,54 @@ class Dataset():
             news_urls = flatten([ [self.get_links(date,'news') for date in tqdm(new_timestamps,total=len(new_timestamps))],  [self.get_links(date,'worldnews') for date in tqdm(new_timestamps,total=len(new_timestamps))]   ])
             climateskeptics_urls =  [self.get_links(date,'climateskeptics') for date in tqdm(new_timestamps,total=len(new_timestamps))]
 
-            df_climate = pd.DataFrame(flatten(climate_urls),columns= ['author','timestamp','post_url','media_url','score'])
-            df_news = pd.DataFrame(flatten(news_urls),columns= ['author','timestamp','post_url','media_url','score'])
-            df_skeptics = pd.DataFrame(flatten(climateskeptics_urls),columns= ['author','timestamp','post_url','media_url','score'])
+            df_climate = pd.DataFrame(flatten(climate_urls),columns= ['author','timestamp','post_url','media_url','score','comments','uuid'])
+            df_news = pd.DataFrame(flatten(news_urls),columns= ['author','timestamp','post_url','media_url','score','comments','uuid'])
+            df_skeptics = pd.DataFrame(flatten(climateskeptics_urls),columns= ['author','timestamp','post_url','media_url','score','comments','uuid'])
 
 
             df_climate['article'] = simple_map(self.get_articles, df_climate['media_url'])
             df_news['article'] = simple_map(self.get_articles, df_news['media_url'])
             df_skeptics['article'] = simple_map(self.get_articles, df_skeptics['media_url'])
 
-            df_climate.index = np.arange(len(self.df_climate), len(df_climate) + len(self.df_climate))
-            df_news.index = np.arange(len(self.df_news), len(df_news) + len(self.df_news))
-            df_skeptics.index = np.arange(len(self.df_skeptics), len(df_skeptics) + len(self.df_skeptics))
+
+            df_climate['uuid'] = df_climate.apply(lambda x: uuid(x[['post_url']]),axis = 1)
+            df_news['uuid'] = df_news.apply(lambda x: uuid(x[['post_url']]),axis = 1)
+            df_skeptics['uuid'] = df_skeptics.apply(lambda x: uuid(x[['post_url']]), axis = 1)
+
+            df_climate.set_index(['uuid'], inplace=True)
+            df_news.set_index(['uuid'], inplace=True)
+            df_skeptics.set_index(['uuid'], inplace=True)
 
             self.df_climate = pd.concat([self.df_climate,df_climate])
             self.df_news = pd.concat([self.df_news,df_news])
             self.df_skeptics = pd.concat([self.df_skeptics,df_skeptics])
+
+            df_sentence = pd.concat([df_climate,df_news,df_skeptics])
+            df_sentence = df_sentence[df_sentence['article'] != None]
+            df_sentence['sentence'] = df_sentence['article'].apply(lambda x: sent_token(x))
+            df_sentence['new_ind'] = df_sentence['sentence'].apply(lambda x: [i for i in range(len(x))])
+            df_sentence['parent'] = df_sentence.index
+            df_sentence = df_sentence.explode(['sentence','new_ind'])
+            df_sentence['uuid'] = df_sentence.apply(lambda x: uuid(x[['new_ind','parent']]), axis =1)
+            df_sentence.set_index(['uuid'], inplace=True)
+            self.df_sentence = pd.concat([self.df_sentence,df_sentence[['parent','sentence']]])
+
+            
 
             self.save()
 
 
         
     def get_links(self,date_int, subreddit):
-        url = 'https://api.pushshift.io/reddit/search/submission/?subreddit='+ subreddit +'&sort=desc&sort_type=score&after=' + str(int(date_int)) + '&before='+str(int(date_int + 86400)) +'&size=10000'
-        print(url)
+        url = 'https://api.pushshift.io/reddit/search/submission/?subreddit='+ subreddit +'&sort=asc&sort_type=num_comments&after=' + str(int(date_int)) + '&before='+str(int(date_int + 86400)) +'&size=50'
         res = None
         while res != 200:
             r = requests.get(url)
             res = r.status_code
-            print(res)
             sleep(1)
         data = r.json()['data']
 
-        return [{'author':x['author'],'timestamp': x['created_utc'],'post_url':x['full_link'], 'media_url':x['url'], 'score':x['score']} for x in data]
+        return [{'author':x['author'],'timestamp': x['created_utc'],'post_url':x['full_link'], 'media_url':x['url'], 'score':x['score'], 'comments':x['num_comments']} for x in data]
 
 
     def get_articles(self,url):
