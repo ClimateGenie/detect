461c613cf6b4fe5a1ff300a5b15b15550f911a91
Fonzzy1 <alfiechadwick@hotmail.com>
2022-06-01 14:20:37 +1000

    better eval

--- evaluate.py
+++ evaluate.py
@@ -48,46 +48,63 @@ threshold_parms = [0.1 *x for x in range(5,11)]
 embedding_parms = ['doc2vecdm','doc2vecdbow','tfidf','bow','word2vecsum', 'word2vecmean']
 author_info_parms = [True, False]
 predictive_model_parms = ['ExtraTree','DecisionTree','SGDClassifier','RidgeClassifier','PassiveAggressiveClassifier','AdaBoostClassifier','GradientBoostingClassifier', 'BaggingClassifier','ExtraTreeClassifier','RandomForestClassifier','BernoulliNB','LinearSVC','LogisticRegression','LogisticRegressionCV','MultinomialNB','NearestCentroid','NuSVC','Perceptron','SVC']
-unlabled_frac = [2**(-x) for x in range(8,11)]
+unlabled_frac = [2**(-x) for x in range(0,12)]
 labeled_frac = [0.1*x for x in range(1,10)] ## Will need a minimum of 10% for testing
 
-df_eval = pd.DataFrame(columns=['min_count','model_size','alpha','threshold','embedding_parms','author_info','predictive_model_parms','unlabled','labeled', 'model'])
+df_eval = pd.DataFrame(columns=['min_count','model_size','alpha','threshold','embedding_parms','author_info','predictive_model_parms','unlabled','labeled', 'filter', 'embedding','model'])
 
 labeled_data, unlabled_data = [x for _, x in data.groupby(data['class']==-1)]
 
-## First build the various traioning sets
+
+"""
+What Questions do we want to answer?
+
+Basic Tuning:
+ - What is the best filter
+ - What is the best embedding scheme + model combo
+
+Is it worthwhile to gather more data:
+ - Does the model improve with more unlabled_data
+ - Does the model improve with more labeled_data
+ - Does author_info improve the model?
+ - Do I even need a filter -> is embedding scheme improved by more test overall
+ - If inconclusive -> is there a tradeoff between the best filter and the best model
+ - Is there an ensemble model that improves on indivisual model
+
+"""
+
+## What is the best filter
+"""
+We itterate through the internal parts of the filter using grid search
+Use all data as this is speedy af
+
+"""
+
+
+
+train = data.copy()
 i = 0
-total = prod([len(min_count_parms), len(model_size_parms),len(alpha_parms),len(threshold_parms),len(unlabled_frac),len(labeled_frac)])
-print(total)
-for u in unlabled_frac:
-    train_unlabeled = unlabled_data.sample(frac=u, random_state=1)
-    for l in labeled_frac:
-        train_labeled, test = train_test_split(labeled_data,train_size = l, random_state=1)
-        train = pd.concat([train_labeled, train_unlabeled])
-        ## Now initialise a filter
-        for min_count in min_count_parms:
-            for alpha in alpha_parms:
-                ## Due to how the filter is built, it can be trained once and altered each time for treashold and model_size
-                f = Filter({'min_count':min_count,'alpha':alpha})
-                f.train(train[train['weak_climate']]['sentence'],train[~train['weak_climate']]['sentence'])
-                for model_size in model_size_parms:
-                    f.model_size = model_size
-                    indecies = f.stat.sort_values('score',ascending = False).iloc[0:f.model_size,].index
-                    f.model = f.norm.loc[indecies]
-                    for threshold in threshold_parms:
-                        f.threshold = threshold
-                        train['climate'] = f.predict(train)
-                        for embedding_scheme in embedding_parms:
-                            for author_info in author_info_parms:
-                                e = Embedding(model_type=embedding_scheme,author_info=author_info)
-                                e.train(train[train['climate']])
-                                train['vector']= e.predict(train[train['climate']])
-                                for predictive_model in predictive_model_parms:
-                                    m = Predictive_model(model=predictive_model)
-                                    m.train(train[train['climate']])
-                                    model = Model()
-                                    model.filter = f
-                                    model.embedding_scheme = e
-                                    model.predictive_model = m
-                                    df_eval.loc[len(df_eval)] = [min_count,model_size,alpha,threshold,embedding_scheme,author_info,predictive_model,len(train_unlabeled), len(train_labeled), model]
+total = prod([len(min_count_parms), len(model_size_parms),len(alpha_parms),len(threshold_parms)])
+
+# Create all of the filters
+for min_count in min_count_parms:
+    for alpha in alpha_parms:
+	## Due to how the filter is built, it can be trained once and altered each time for treashold and model_size
+        f = Filter({'min_count':min_count,'alpha':alpha})
+        f.train(train[train['weak_climate']]['sentence'],train[~train['weak_climate']]['sentence'])
+        for model_size in model_size_parms:
+            f.model_size = model_size
+            indecies = f.stat.sort_values('score',ascending = False).iloc[0:f.model_size,].index
+            f.model = f.norm.loc[indecies]
+            for threshold in threshold_parms:
+                f.threshold = threshold
+                df_eval.loc[len(df_eval)] = [min_count,model_size,alpha,threshold,None,None,None,len(unlabled_data),len(labeled_data), copy(f),None,None]
+                print(df_eval)
+
+test = pd.read_csv('https://www.sustainablefinance.uzh.ch/dam/jcr:ed47e4e1-353a-42cc-9f2e-0f199b85815a/Wiki-Doc-Dev.tsv', sep = '\t')
+
+
+
+
+
 
